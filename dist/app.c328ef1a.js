94b4ebe76018323f939cfbc9998b9077245ee376
Cameron Sanders <csanders@protonmail.com>
2019-02-19 14:41:16 -0600

    finish setup

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -32983,8 +32983,74 @@ var THREE = _interopRequireWildcard(require("three"));
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
 
-console.log(THREE);
-},{"three":"../node_modules/three/build/three.module.js"}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var container = {
+  el: document.querySelector('#scene'),
+  height: function height() {
+    return this.el.getBoundingClientRect().height;
+  },
+  width: function width() {
+    return this.el.getBoundingClientRect().width;
+  }
+};
+
+function configScene(scene, camera) {
+  var box = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({
+    color: 0x00ff00
+  }));
+  var plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), new THREE.MeshBasicMaterial({
+    color: 0xff0000,
+    side: THREE.DoubleSide
+  }));
+  box.name = 'mybox';
+  plane.name = 'myplane';
+  scene.add(box);
+  scene.add(plane);
+  plane.rotation.x += THREE.Math.degToRad(90);
+  box.position.y += box.geometry.parameters.height * 0.5;
+  camera.position.x = 1;
+  camera.position.y = 2;
+  camera.position.z = 5;
+  camera.lookAt(box.position.x, box.position.y, box.position.z);
+}
+
+function init(container, configScene) {
+  var scene = new THREE.Scene();
+  var cam = {
+    fov: 55,
+    aspect: container.width() / container.height(),
+    near: 0.1,
+    far: 1000
+  };
+  var camera = new THREE.PerspectiveCamera(cam.fov, cam.aspect, cam.near, cam.far);
+  configScene(scene, camera);
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setSize(container.width(), container.height());
+  container.el.appendChild(renderer.domElement);
+  return {
+    scene: scene,
+    camera: camera,
+    renderer: renderer
+  };
+}
+
+function render(init) {
+  var scene = init.scene,
+      camera = init.camera,
+      renderer = init.renderer;
+  var box = scene.getObjectByName('mybox');
+  var plane = scene.getObjectByName('myplane');
+  box.rotation.z += 0.005;
+  plane.rotation.z -= 0.005;
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(function () {
+    render(init);
+  });
+}
+
+var environment3d = init(container, configScene);
+render(environment3d);
+console.log(environment3d);
+},{"three":"../node_modules/three/build/three.module.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -33011,7 +33077,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57565" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33013" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
@@ -33153,5 +33219,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},["../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","app.js"], null)
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","app.js"], null)
 //# sourceMappingURL=/app.c328ef1a.map
--- src/app.js
+++ src/app.js
@@ -1,2 +1,70 @@
 import * as THREE from 'three'
-console.log(THREE)
+
+const container = {
+  el: document.querySelector('#scene'),
+  height () {
+    return this.el.getBoundingClientRect().height
+  },
+  width () {
+    return this.el.getBoundingClientRect().width
+  }
+}
+
+function configScene (scene, camera) {
+  const box = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+  )
+  const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(5, 5),
+    new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide })
+  )
+  box.name = 'mybox'
+  plane.name = 'myplane'
+  scene.add(box)
+  scene.add(plane)
+  plane.rotation.x += THREE.Math.degToRad(90)
+  box.position.y += box.geometry.parameters.height * 0.5
+
+  camera.position.x = 1
+  camera.position.y = 2
+  camera.position.z = 5
+  camera.lookAt(box.position.x, box.position.y, box.position.z)
+}
+
+function init (container, configScene) {
+  const scene = new THREE.Scene()
+  const cam = {
+    fov: 55,
+    aspect: container.width() / container.height(),
+    near: 0.1,
+    far: 1000
+  }
+  const camera = new THREE.PerspectiveCamera(
+    cam.fov,
+    cam.aspect,
+    cam.near,
+    cam.far
+  )
+  configScene(scene, camera)
+  const renderer = new THREE.WebGLRenderer()
+  renderer.setSize(container.width(), container.height())
+  container.el.appendChild(renderer.domElement)
+  return { scene, camera, renderer }
+}
+
+function render (init) {
+  const { scene, camera, renderer } = init
+  const box = scene.getObjectByName('mybox')
+  const plane = scene.getObjectByName('myplane')
+  box.rotation.z += 0.005
+  plane.rotation.z -= 0.005
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(() => {
+    render(init)
+  })
+}
+
+const environment3d = init(container, configScene)
+render(environment3d)
+console.log(environment3d)
